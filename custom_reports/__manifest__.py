6367c478bd6667958f252a0861ed79dae0ff61e8
Pamudi1 <162337694+Pamudi1@users.noreply.github.com>
2026-06-12 09:20:54 +0530

    Update __manifest__.py

--- custom_reports/__manifest__.py
+++ custom_reports/__manifest__.py
@@ -1,6 +1,12 @@
-{
-    'name': 'Custom Report',
-    'version': '1.0',
-    'depends': ['base'],
-    'installable': True,
-}
+from odoo import models
+
+class SalesReport(models.AbstractModel):
+    _name = 'report.custom_reports.sales_report_template'
+
+    def _get_report_values(self, docids, data=None):
+        docs = self.env['sale.order'].browse(docids)
+        return {
+            'doc_ids': docids,
+            'doc_model': 'sale.order',
+            'docs': docs,
+        }
