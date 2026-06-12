49a06d3bfa5909820021664736a22e67b0bc023e
Pamudi1 <162337694+Pamudi1@users.noreply.github.com>
2026-06-12 09:23:39 +0530

    Create __manifest__.py

---
+++ custom_reports/report/custom_reports/report/custom_reports/custom_reports/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Custom Report',
+    'version': '1.0',
+    'depends': ['base', 'sale'],
+    'data': [
+        'report/report.xml',
+        'report/report_template.xml',
+    ],
+    'installable': True,
+}
